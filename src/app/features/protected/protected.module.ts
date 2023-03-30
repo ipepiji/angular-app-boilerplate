b00c8b0902f29e4f043bea336f551423d623ad79
Mohd Irfan Hafizhi bin Zulkifli <irfan.zulkifli@tmrnd.com.my>
2023-03-30 11:41:42 +0800

    Initial commit

---
+++ src/app/features/protected/protected.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '@shared/shared.module';
+import { ProtectedRoutingModule } from './protected-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [SharedModule, ProtectedRoutingModule],
+})
+export class ProtectedModule {}
